fab3a7d7eca54dc817947a27193aea1f77c9d911
Akshansh Shrivastava <67007034+akki251@users.noreply.github.com>
2021-09-11 18:20:18 +0000

    Array Update

--- Arrays and Sorting Algos/Find duplicates in O(N) time and 0(1) Space.cpp
+++ Arrays and Sorting Algos/Find duplicates in O(N) time and 0(1) Space.cpp
@@ -1,26 +1,13 @@
-https://leetcode.com/problems/find-all-duplicates-in-an-array/
-https://www.youtube.com/watch?v=ACYunkWQnSI
-
-int absolute(int x)
-{
-    if (x < 0)
-        return -x;
-    else
-        return x;
-}
-
-int a[] = {3, 2, 1, 4, 2};
-    int n = sizeof(a) / sizeof(a[0]);
-    int i, j;
-
-    
-    for (i = 0; i < n; i++)
-    {
-        int index = absolute(a[i]) - 1;
-        if (a[index] < 0)
-        {
-            cout << absolute(a[index]);
-        }
-        a[index] = -a[index];
+ int findDuplicate(vector<int>& nums) {
+        
+         for(int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;
+            
+            // mark as visited
+            nums[index] *= -1;
+            // incase of duplicate, this will become +ve 
+            if(nums[index] > 0)
+                return abs(nums[i]);
+        }    
+        return -1;
     }
-    
